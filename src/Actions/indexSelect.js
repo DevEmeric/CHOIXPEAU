8dba4da6adf720676aaf39513466f1cc36951f62
Emeric <emeric555@gmail.com>
2018-12-09 16:35:22 +0100

    ok

--- src/Actions/indexSelect.js
+++
@@ -1,7 +0,0 @@
-
-export const indexSelect = () => {
-    return {
-    type: 'UPDATE_CHOICE_WITH_MY_INDEX',
-    index: Math.floor(Math.random()*4)
-    }
-}
--- src/Reducer/reducer.js
+++
@@ -1,9 +0,0 @@
-function Choixpeaureducer (state ={selectedHouses: "Serpentard"}, action){
-  switch(action.type) {
-      case "CHOIXPEAU" :
-       return {selectedHouses:  houses[Math.floor(Math.random() * 4)], houses}
-      default: 
-          return state
-  }
-}
-export default Choixpeaureducer;
